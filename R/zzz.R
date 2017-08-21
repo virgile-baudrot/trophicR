4eee7281bd60775346a126669350d882f1b17ed9
vbaudrot <virgile.baudrot@posteo.net>
2017-08-21 15:08:51 +0200

    edit NAMESPACE

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,5 @@
+#' @useDynLib trophicR, .registration = TRUE 
+
 .onLoad <- function(libname, pkgname) {
   modules <- paste0("stan_fit4", names(stanmodels), "_mod")
   for (m in modules) loadModule(m, what = TRUE)
