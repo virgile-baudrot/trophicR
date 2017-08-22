ba5977aa0b960f54c835e147fae6f2ce25b139b8
vbaudrot <virgile.baudrot@posteo.net>
2017-08-22 10:33:32 +0200

    add data set to test fitting functions

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Data very simple structure
+#'
+#' @description Data randomly generated for simple H2 test
+#'
+#' @docType data
+#'
+"data_H2test"
+
+#' Data of red fox (Vulpes vulpes) predation on 2 vole prey species (Arvicola scherman and Microtus arvalis) 
+#'
+#' @description Data randomly generated
+#'
+#' @docType data
+#'
+"fox_Raoul2010"
